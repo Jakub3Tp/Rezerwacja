fbb1a6d31f037fa629f9aff55f00cade7a1667f5
Jakub <jakub.gola2@teb.edu.pl>
2024-05-05 12:46:09 +0200

    95% (brak możliwości anulowania rezerwacji)

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         FlightBookingSystem SystemRezerwacji = new FlightBookingSystem();
-        SystemRezerwacji.DodajLot(new Flight("Wrocław", "Rzym", "20-02-2024", 4,35));
-        SystemRezerwacji.DodajLot(new Flight("Warszawa", "Bolonia", "29-02-2024", 4,35));
+        SystemRezerwacji.DodajLot(new Flight("Krakow", "Niemcy", "02-12-2023", 2,35));
+        SystemRezerwacji.DodajLot(new Flight("Japonia", "Warszawa", "19-03-2024", 14,35));
         SystemRezerwacji.wyswietlania_dostepnych_lotow();
 
-        Flight TanieLoty =  SystemRezerwacji.wyszukiwania_lotow("Wrocław","20 luty").get(0);
+        Flight TanieLoty =  SystemRezerwacji.wyszukiwania_lotow("Włochy","20.02.2024").getFirst();
 
-        SystemRezerwacji.rezerwacja(TanieLoty,"Darian Filman",500 );
+        SystemRezerwacji.rezerwacja(TanieLoty,"Darian Filman",1200 );
 
         SystemRezerwacji.wyswietlania_dostepnych_lotow();
-
-        int NowaRezerwacja =  SystemRezerwacji.wyszukiwania_lotow("Wrocław", "20-02-2024").get(0).getLiczba_dostepnych_miejsc();
-
-        SystemRezerwacji.anulowanie_rezerwacji(NowaRezerwacja);
-        SystemRezerwacji.wyswietlania_dostepnych_lotow();
+        //nie udało się zrobić wszystkiego
     }
 }
